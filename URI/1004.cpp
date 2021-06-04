6d83091c397ecdacfd22573ac8d6a7067f7bd843
Carlos Eduardo <52487636+Cadu-or@users.noreply.github.com>
2021-06-03 22:09:52 -0300

    1004 - Simple Product (C++)

---
+++ URI/1004.cpp
@@ -0,0 +1,17 @@
+/* 1004 - Simple Product */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int PROD, a, b;
+
+    cin >> a >> b;
+
+    PROD = a*b;
+
+    cout << "PROD = " << PROD << endl;
+
+    return 0;
+}
