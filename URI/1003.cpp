93aaddee610555ae1a170303ff66f9be2ececc8a
Carlos Eduardo <52487636+Cadu-or@users.noreply.github.com>
2021-06-14 18:39:34 -0300

    1003 - Simple Sum (C++)

---
+++ URI/1003.cpp
@@ -0,0 +1,13 @@
+/* Problem: 1003 - Simple Sum */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int SOMA, a, b;
+    cin >> a >> b;
+    SOMA = a + b;
+    cout << "SOMA = " << SOMA << endl;
+    
+    return 0;
+}
