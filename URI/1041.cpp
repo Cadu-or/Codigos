69b56f67964239369d12466913c036df67cb36d6
Carlos Eduardo <52487636+Cadu-or@users.noreply.github.com>
2021-06-16 11:16:29 -0300

    1041 - Coordinates of a Point (C++)

---
+++ URI/1041.cpp
@@ -0,0 +1,37 @@
+/* Problem: 1041 - Coordinates of a Point */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    float X, Y;
+
+    cin >> X >> Y;
+
+    if(X == 0 && Y == 0){
+        cout << "Origem" << endl;
+
+    }else if(X == 0 && Y != 0){
+        cout << "Eixo Y" << endl;
+
+    }else if(Y == 0 && X != 0){
+        cout << "Eixo X" << endl;
+
+    }else if(Y > 0 && X > 0){
+        cout << "Q1" << endl;
+
+    }else if(Y > 0 && X < 0){
+        cout << "Q2" << endl;
+
+    }else if(Y < 0 && X > 0){
+        cout << "Q4" << endl;
+
+    }else if(Y < 0 && X < 0){
+        cout << "Q3" << endl;
+    }
+
+
+    return 0;
+}
