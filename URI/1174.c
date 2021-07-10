de2479bc6a8bc05acd585ec166be40c426d1756e
Carlos Eduardo <52487636+Cadu-or@users.noreply.github.com>
2021-07-10 17:40:27 -0300

    1174 - Array Selection I (C)

---
+++ URI/1174.c
@@ -0,0 +1,17 @@
+/* Problem: 1174 - Array Selection I */
+
+#include <stdio.h>
+
+int main(){
+    float A[100];
+    int i;
+
+    for(i=0; i<100; i++){
+        scanf("%f", &A[i]);
+        if (A[i]<= 10){
+            printf ("A[%d] = %.1f\n", i, A[i]);
+        }
+    }
+
+    return 0;
+}
